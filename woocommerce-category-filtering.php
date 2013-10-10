66bca199d77cd0e7492a9480c3fd19e52dad6ddc
Marcel Eschmann <marcel.eschmann@gmail.com>
2013-10-11 00:19:44 +0200

    Template override

--- woocommerce-category-filtering.php
+++ woocommerce-category-filtering.php
@@ -56,37 +56,16 @@ function wooccf_cssclass() {
 	}*/
 }
 
-
-
 function wooccf_template_override( $template, $template_name, $template_path ) {
 	global $woocommerce;
 
-	$_template = $template;
-	if ( ! $template_path ){
-		$template_path = $woocommerce->template_url;
-	}
+	$path = untrailingslashit(plugin_dir_path( __FILE__ )).'/woocommerce/';
 
-	$plugin_path  = untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/woocommerce/';
-
-	// Look within passed path within the theme - this is priority
-	$template = locate_template(
-		array(
-			$template_path . $template_name,
-			$template_name
-		)
-	);
-
-	// Modification: Get the template from this plugin, if it exists
-	if ( ! $template && file_exists( $plugin_path . $template_name ) ) {
+	// Override template if existing
+	if ( file_exists( $plugin_path . $template_name ) ) {
 		$template = $plugin_path . $template_name;
 	}
-	
-	// Use default template
-	if ( ! $template ) {
-		$template = $_template;
-	}
-	
-	// Return what we found
+
 	return $template;
 }
 add_filter( 'woocommerce_locate_template', 'wooccf_template_override', 10, 3 );
